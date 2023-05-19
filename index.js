45f9b49b5861f41b44276d5fa4651ea11babcdcf
VictorMPstudent <victor.medina.perez@estudiantat.upc.edu>
2023-05-19 19:42:19 +0200

    Change title for winner and title too big fix

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ let randomNumberP2 = Math.floor(Math.random()*6+1);
 
 document.querySelector(".img1").setAttribute("src", "Images/dice"+randomNumberP1+".png");
 document.querySelector(".img2").setAttribute("src", "Images/dice"+randomNumberP2+".png");
+
+if(randomNumberP1 > randomNumberP2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 is the winner";
+}
+else if(randomNumberP1 < randomNumberP2){
+    document.querySelector("h1").innerHTML = "Player 2 is the winner🚩";
+}
+else{
+    document.querySelector("h1").innerHTML = "🚩Draw🚩";
+}
