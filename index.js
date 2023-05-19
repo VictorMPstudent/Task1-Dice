f3d178a2d66425234f7bab93f9b409982064f3d4
VictorMPstudent <victor.medina.perez@estudiantat.upc.edu>
2023-05-19 19:39:06 +0200

    Random images and import them on html (need it)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let randomNumberP1 = Math.floor(Math.random()*6+1);
 let randomNumberP2 = Math.floor(Math.random()*6+1);
 
-console.log(randomNumberP1);
-console.log(randomNumberP2);
+document.querySelector(".img1").setAttribute("src", "Images/dice"+randomNumberP1+".png");
+document.querySelector(".img2").setAttribute("src", "Images/dice"+randomNumberP2+".png");
