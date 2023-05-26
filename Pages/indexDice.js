97804134c79a59a8ed9c6383d1865d457c9e4029
VictorMPstudent <victor.medina.perez@estudiantat.upc.edu>
2023-05-26 10:41:09 +0200

    Change Dice files to make new page

--- Pages/indexDice.js
+++ Pages/indexDice.js
@@ -1,8 +1,8 @@
 let randomNumberP1 = Math.floor(Math.random()*6+1);
 let randomNumberP2 = Math.floor(Math.random()*6+1);
 
-document.querySelector(".img1").setAttribute("src", "Images/dice"+randomNumberP1+".png");
-document.querySelector(".img2").setAttribute("src", "Images/dice"+randomNumberP2+".png");
+document.querySelector(".img1").setAttribute("src", "../Images/dice"+randomNumberP1+".png");
+document.querySelector(".img2").setAttribute("src", "..//Images/dice"+randomNumberP2+".png");
 
 if(randomNumberP1 > randomNumberP2){
     document.querySelector("h1").innerHTML = "🚩Player 1 is the winner";
